49773dbab582d3d8317b186dec72ad4900eb7f09
MysticBoy <mysticboy@live.com>
2020-10-09 16:11:27 +0800

    完整项目极其目录

---
+++ DiscuzX.Net/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+
+namespace DiscuzX.Server
+{
+    class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Adds a default in-memory implementation of IDistributedCache.
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+            });
+
+            services.AddPhp(options =>
+            {
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSession();
+
+            app.UsePhp();
+            app.UseStaticFiles(new StaticFileOptions { FileProvider = new PhysicalFileProvider(Path.GetFullPath("../upload/")) });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
---
+++ DiscuzX.Net/wwwroot/uc_server/data/config.inc.php
@@ -0,0 +1,18 @@
+<?php 
+define('UC_DBHOST', '127.0.0.1');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', 'kissme');
+define('UC_DBNAME', 'ultrax');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', 'pre_ucenter_');
+define('UC_COOKIEPATH', '/');
+define('UC_COOKIEDOMAIN', '');
+define('UC_DBCONNECT', 0);
+define('UC_CHARSET', 'utf-8');
+define('UC_FOUNDERPW', 'ace214be021fbb7dd00d6658fd94cd8e');
+define('UC_FOUNDERSALT', 'a2Zbk1');
+define('UC_KEY', 't2dbV1Na3d00T1J1L5KeH0L1CbW3X5h3N6MdCdy4saycXbx8b3Q5s4NfJ3U7s982');
+define('UC_SITEID', 'f2ZbT1VaVdk0N1A1A51ee0N1obi3l5k316Rd9dI4oalcFba8Z3m5q4Pf03j709V2');
+define('UC_MYKEY', '72Fbc1Calda0g1c1C5deJ0K1TbF3D543u6ad0du4yaWckbH833J5V4ffT3Y7V9l2');
+define('UC_DEBUG', false);
+define('UC_PPP', 20);
